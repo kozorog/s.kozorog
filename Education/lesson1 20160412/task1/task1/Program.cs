0475b250318c8404bcf87c9334b541fc00510093
kozorog <s.kozoorg@2gis.ru>
2016-04-13 22:52:32 +0600

    домашка от 12 апреля

--- Education/lesson1 20160412/task1/task1/Program.cs
+++ Education/lesson1 20160412/task1/task1/Program.cs
@@ -11,31 +11,50 @@ namespace task1
         static void Main(string[] args)
         {
             //задание 1
-            Console.WriteLine("Задание 1. Ввести с консоли 2 числа. Вывести их сумму на контроль");
-            string t1 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 1
-            string t2 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 2
-            int t11 = Convert.ToInt32(t1); //преобразуем переменную в int
-            int t22 = Convert.ToInt32(t2); //преобразуем переменную в int
-            int t3 = t11 + t22; // складываем
-            Console.WriteLine(t3); //выводим
+            Console.WriteLine("Задание 1. Ввести с консоли 2 числа. Вывести их сумму на конcоль");
+            Console.WriteLine("Введите первое число");
+            string number1 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 1
+            Console.WriteLine("Введите второе число");
+            string number2 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 2
+            int num1 = Convert.ToInt32(number1); //преобразуем переменную в int
+            int num2 = Convert.ToInt32(number2); //преобразуем переменную в int
+            int sum = num1+num2; // складываем
+            Console.WriteLine("Суммируем. Получили:");
+            Console.WriteLine(sum); //выводим
+            Console.ReadLine(); //типо пауза
 
             //задание 2
-            Console.WriteLine("Задание 1. Ввести с консоли 2 числа n и j. Вывести значение i-го бита числа n");
-            string b1 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 1
-            string b2 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 2
-            int b11 = Convert.ToInt32(b1); //преобразуем переменную в int
-            int b22 = Convert.ToInt32(b2); //преобразуем переменную в int
-            int b3 = b11 + b22; // складываем
-            Console.WriteLine(b3); //выводим
+            Console.WriteLine("Задание 2");
+            Console.WriteLine( "Ввести с консоли 2 числа n и i. Вывести значение i-го бита числа n");
 
-            //задание 2
-            Console.WriteLine("Задание 1. Ввести с консоли 2 числа n и j. Вывести значение i-го бита числа n");
-            string b1 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 1
-            string b2 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 2
-            int b11 = Convert.ToInt32(b1); //преобразуем переменную в int
-            int b22 = Convert.ToInt32(b2); //преобразуем переменную в int
-            int b3 = b11 + b22; // складываем
-            Console.WriteLine(b3); //выводим
+            string str_n = Console.ReadLine(); //объявляем и считываем с консоли  переменную 1
+            string str_i = Console.ReadLine(); //объявляем и считываем с консоли  переменную 2
+
+            int n = Convert.ToInt32(str_n); //преобразуем переменную в int
+            int i = Convert.ToInt32(str_i); //преобразуем переменную в int
+            n = n >> i; //сдвигаем;
+            n = n & 1; //
+            Console.WriteLine("Ответ:");
+            Console.WriteLine(n); //выводим 
+                         
+            Console.ReadLine(); //типо пауза
+
+
+            //задание 3
+            Console.WriteLine("Задание 3");
+            Console.WriteLine("Ввести с консоли число. Обнулить последний бит этого числа. Вывести на консоль");
+
+          // не сделал. может завтра
+            
+
+            //int m = Convert.ToInt32(i); //преобразуем переменную в int
+            
+            //m = a >> aa; //сдвигаем;
+            //int ccc = a & 1; //
+            //Console.WriteLine(a); //выводим
+
+
+
         }
     }
 }
