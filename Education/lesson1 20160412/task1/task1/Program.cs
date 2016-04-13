4f035b2f5cf862c9e4deca5ce9252ef338a10987
kozorog <s.kozoorg@2gis.ru>
2016-04-13 16:57:56 +0600

    task1

---
+++ Education/lesson1 20160412/task1/task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задание 1
+            Console.WriteLine("Задание 1. Ввести с консоли 2 числа. Вывести их сумму на контроль");
+            string t1 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 1
+            string t2 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 2
+            int t11 = Convert.ToInt32(t1); //преобразуем переменную в int
+            int t22 = Convert.ToInt32(t2); //преобразуем переменную в int
+            int t3 = t11 + t22; // складываем
+            Console.WriteLine(t3); //выводим
+
+            //задание 2
+            Console.WriteLine("Задание 1. Ввести с консоли 2 числа n и j. Вывести значение i-го бита числа n");
+            string b1 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 1
+            string b2 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 2
+            int b11 = Convert.ToInt32(b1); //преобразуем переменную в int
+            int b22 = Convert.ToInt32(b2); //преобразуем переменную в int
+            int b3 = b11 + b22; // складываем
+            Console.WriteLine(b3); //выводим
+
+            //задание 2
+            Console.WriteLine("Задание 1. Ввести с консоли 2 числа n и j. Вывести значение i-го бита числа n");
+            string b1 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 1
+            string b2 = Console.ReadLine(); //объявляем и считываем с консоли  переменную 2
+            int b11 = Convert.ToInt32(b1); //преобразуем переменную в int
+            int b22 = Convert.ToInt32(b2); //преобразуем переменную в int
+            int b3 = b11 + b22; // складываем
+            Console.WriteLine(b3); //выводим
+        }
+    }
+}
