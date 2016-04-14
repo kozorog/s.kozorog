d3db8af387e6734341052c466b71e1c16e119877
kozorog <ksn_1976@mail.ru>
2016-04-14 22:36:08 +0600

    переделал структуру папок в гите

---
+++ lesson2 20160412/task4/task4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str_N = Console.ReadLine();
+            int N = Convert.ToInt32(str_N);
+
+            ghgh
+            int[,] matr1 = new int[N, N];
+            for (int i=0;i< N;i++)
+            { }
+        }
+    }
+}
