aff523d0bc5195edfaf9c8124bd26235440668f9
kozorog <s.kozoorg@2gis.ru>
2016-04-14 17:04:57 +0600

    lesson2

---
+++ Education/lesson2 20160412/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str_n = Console.ReadLine();
+            int n = Convert.ToInt32(str_n);
+            int sum=0;
+            int max=0;
+            for (int i=0; i < n; i++)
+            
+            {
+               string str_t = Console.ReadLine();
+                int t = Convert.ToInt32(str_t);
+                sum = sum + t; //+= t
+                 }
+
+
+            Console.WriteLine("Сумма");
+            Console.WriteLine(sum);
+
+
+
+        }
+
+    }
+}
