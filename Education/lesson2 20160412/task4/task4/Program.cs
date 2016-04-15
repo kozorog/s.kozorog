6e3f3c383c7e83e32c5e63b7a099eae5420bcf11
kozorog <s.kozoorg@2gis.ru>
2016-04-15 09:17:12 +0600

    тестовый комит

--- Education/lesson2 20160412/task4/task4/Program.cs
+++ Education/lesson2 20160412/task4/task4/Program.cs
@@ -13,7 +13,7 @@ namespace task4
             string str_N = Console.ReadLine();
             int N = Convert.ToInt32(str_N);
 
-            ghgh
+       
             int[,] matr1 = new int[N, N];
             for (int i=0;i< N;i++)
             { }
