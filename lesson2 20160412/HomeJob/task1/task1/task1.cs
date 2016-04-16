272f5a06239315a63628ec1a115b402a7265af4a
kozorog <ksn_1976@mail.ru>
2016-04-16 15:06:37 +0600

    домашка

---
+++ lesson2 20160412/HomeJob/task1/task1/task1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Заполнить массив длиной N случайными числами. Ввести с консоли число A. Вывести Yes, если число A есть в массиве, No - в противном случае.
+
+            Console.WriteLine("Введите длину массива N ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число А ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+         
+            Random rand = new Random();
+            Int32[] mass = new Int32[N];
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rand.Next(10); //ограничился 100000 
+            }
+
+            
+            //Вывод на консоль 
+            Console.WriteLine("Массив");
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine(mass[i]);
+            }
+
+
+
+
+
+            foreach (int ii in mass)
+            {
+                
+                                if (ii  == A)
+                {
+
+                    Console.WriteLine(ii);
+                    Console.WriteLine("YES");
+                    break;
+                    
+                }
+                else
+                {
+                    //Console.WriteLine("NO");
+                    continue;
+                    
+                }
+                            
+            
+            }
+
+
+
+        }
+    }
+}
+
