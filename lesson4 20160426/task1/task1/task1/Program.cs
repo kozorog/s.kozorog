4ceda7d7134d57f668ed8484af94ad9e502fb713
kozorog <ksn_1976@mail.ru>
2016-04-27 22:57:08 +0600

    задание 1 выполнено

---
+++ lesson4 20160426/task1/task1/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // *Создать класс “Телефон”, который содержит следующие данные: код города,
+            //*номер телефона, которые не могут быть изменены.Добавить метод,
+            //*который возвращает строку вида “(999) 999999” или “999999”, 
+            //*если код города отсутствует.
+            
+
+            PhoneNumber p = new PhoneNumber("", "999999");
+            Console.WriteLine($"Полный номер телефона {p.GetNumber()}");
+
+
+        }
+    }
+}
