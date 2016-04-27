8c24a00e9d46d069b7d7b2619d0c31294d40df30
kozorog <ksn_1976@mail.ru>
2016-04-27 22:59:34 +0600

    начало работы над заданием2

---
+++ lesson4 20160426/task2/task2/task1/Phone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class PhoneNumber
+    {
+
+        private string _code;
+        private string _phone;
+
+        public string GetNumber()
+        {
+            if (_code == null || _code =="")
+            {
+                return _phone;
+            }
+            else
+                return "("+_code+")" + _phone;
+         
+        } 
+                
+    //конструктор
+    public PhoneNumber(string code, string phone)
+    {
+        _code = code;
+        _phone = phone;
+    }
+        public PhoneNumber(string phone) : this (null,phone)
+        {
+           
+        }
+
+    }
+
+}
