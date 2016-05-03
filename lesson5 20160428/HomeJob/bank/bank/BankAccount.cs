bbfb6510c96696c3682b1753115acfc65394caea
kozorog <ksn_1976@mail.ru>
2016-05-03 21:47:07 +0600

    домашка. начало.

---
+++ lesson5 20160428/HomeJob/bank/bank/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    class BankAccounts
+    {
+        int NumberAccount;
+        string OwnerAccount;
+        float CurrentSumm;
+        //создаем конструктор
+        public BankAccounts (int numberAccount, string ownerAccount, float currentSumm)
+            {
+            NumberAccount = numberAccount;
+            OwnerAccount = ownerAccount;
+            CurrentSumm = currentSumm;
+
+            }
+    }
+}
